853340ff45372529323451525e59592bc82884a8
Cghernandezvivar <cghernandezvivar@mail.csuchico.edu>
2026-05-03 23:41:58 -0700

    Added post.service.js

--- server/services/post.service.js
+++ server/services/post.service.js
@@ -1,8 +1,9 @@
 import Post from '../models/post.model'
-import formidable from 'formidable'
+//import formidable from 'formidable'
+import uploadService from './upload.service'
 import fs from 'fs'
 
-const createPost = (req) =>
+/*const createPost = (req) =>
     new Promise((resolve, reject) => {
         let form = new formidable.IncomingForm()
         form.keepExtensions = true
@@ -35,6 +36,32 @@ const createPost = (req) =>
             }
         })
     })
+*/
+
+const createPost = async (req) => {
+    try {
+        const { fields, file } = await uploadService.parseMultipartForm(
+        req,
+        'Image could not be uploaded'
+    )
+        let post = new Post(fields)
+        post.postedBy = req.profile
+
+        if (files.photo) {
+            post.photo.data = await fs.promises.readFile(files.photo.path)
+            post.photo.contentType = files.photo.type
+        }
+        let result = await post.save()
+        return result
+    } catch (err) {
+        if (err.status) throw err
+
+        throw {
+            error: err,
+            status: 400
+        }
+    }
+}
 
 const removePost = async (post) => {
     try {
